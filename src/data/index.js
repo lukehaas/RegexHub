819ceeb6931965610a3875a235b16681abc3e7b6
David J. Bradshaw <david.j.bradshaw@gmail.com>
2017-03-31 10:19:05 +0100

    Add full UK postcode including the strange ones.

--- src/data/index.js
+++ src/data/index.js
@@ -149,6 +149,12 @@ export const patterns = [{
 	tags:"address, postal, zip"
 },
 {
+	name:"UK Postal Code",
+	regex:/^(([gG][iI][rR] {0,}0[aA]{2})|(([aA][sS][cC][nN]|[sS][tT][hH][lL]|[tT][dD][cC][uU]|[bB][bB][nN][dD]|[bB][iI][qQ][qQ]|[fF][iI][qQ][qQ]|[pP][cC][rR][nN]|[sS][iI][qQ][qQ]|[iT][kK][cC][aA]) {0,}1[zZ]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/,
+	description:"Matches all UK postcodes",
+	tags:"address, postal, zip"
+},
+{
 	name:"Brazilian ZIP/Postal Code",
 	regex:/^[0-9]{5}-[0-9]{3}$/,
 	description:"Matches BR ZIP/Postal Code",
